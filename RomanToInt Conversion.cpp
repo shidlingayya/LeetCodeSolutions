982bd40e1ab884af2df57cc3384e82dc28d367a8
Shidlingayya <65803331+shidlingayya@users.noreply.github.com>
2022-08-11 15:50:10 +0530

    Add files via upload

---
+++ RomanToInt Conversion.cpp
@@ -0,0 +1,39 @@
+#include <iostream> 
+#include <vector>
+#include <map>
+int romanToInt(std::string& str)
+{
+    std::map<char, int> symToInt;
+    symToInt.insert({ 'I', 1 });
+    symToInt.insert({ 'V', 5 });
+    symToInt.insert({ 'X', 10 });
+    symToInt.insert({ 'L', 50 });
+    symToInt.insert({ 'C', 100 });
+    symToInt.insert({ 'D', 500 });
+    symToInt.insert({ 'M', 1000 });
+    
+    int sum = 0;
+    uint32_t i = 0U;
+    for (i = 0; i < str.length(); i++)
+    {
+        /*If present value is less than next value,
+          subtract present from next value and add the
+          resultant to the sum variable.
+          CM ==> 900 case
+          */
+        if (symToInt[str[i]] > symToInt[str[i + 1]])
+        {
+            sum += symToInt[str[i + 1]] - symToInt[str[i]];
+            i++;
+            continue;
+        }
+        sum = +symToInt[str[i]];
+    }
+    return sum;
+}
+
+int main(){ 
+    std::string str = "MCMXCIV";
+    std::cout << "getRoman(" << str << ")" << romanToInt(str) << std::endl;
+    return 0;
+}
